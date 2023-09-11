f380d75a33106b3de15f9cf4b09f30ee0718818b
TarlanSB <tarlanworkmail@gmail.com>
2023-09-11 23:28:27 +0300

    Введена статическая переменная size, изменена логика методов

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,51 +1,38 @@
 import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    static int size;
 
     void clear() {
-        Arrays.fill(storage, 0, size(), null);
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     int size() {
-        int count = 0;
-        for (Resume resume : storage) {
-            if (resume != null) {
-                count++;
-            }
-        }
-        return count;
+        return size;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                break;
-            }
-        }
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
-        for (Resume resume : storage) {
-            if (resume != null) {
-                if (Objects.equals(resume.uuid, uuid)) {
-                    return resume;
-                }
-            }
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid))
+                return storage[i];
         }
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                System.arraycopy(storage, i, storage, i, size() - 1);
-                storage[size() - 1] = null;
+                storage[i] = storage[size - 1];
+                storage[size--] = null;
                 break;
             }
         }
@@ -55,6 +42,6 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size());
+        return Arrays.copyOf(storage, size());
     }
 }
