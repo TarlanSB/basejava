6326eb1fc84dbbc626c0a79f163e92d5bf4e7ed5
TarlanSB <tarlanworkmail@gmail.com>
2023-10-09 01:20:35 +0300

    Реализация паттерна "Шаблонный метод"

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -2,15 +2,19 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
+import java.util.Arrays;
+
 public abstract class AbstractArrayStorage implements Storage {
     protected static final int STORAGE_LIMIT = 10000;
     protected final Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size;
 
+    @Override
     public int size() {
         return size;
     }
 
+    @Override
     public Resume get(String uuid) {
         int index = getIndex(uuid);
         if (index >= 0) {
@@ -24,6 +28,30 @@ public abstract class AbstractArrayStorage implements Storage {
         System.out.println("Такого резюме с uuid \"" + uuid + "\" нет");
     }
 
-
     protected abstract int getIndex(String uuid);
+
+    @Override
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    @Override
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    @Override
+    public final void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index >= 0) {
+            storage[index] = r;
+            System.out.println("Резюме с uuid \"" + r.getUuid() + "\" обновилось");
+        } else {
+            info(r.getUuid());
+        }
+    }
 }
