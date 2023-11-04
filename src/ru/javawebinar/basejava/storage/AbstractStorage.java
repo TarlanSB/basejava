3e6ca550586ecff3f59135d16bc1fc04e731238c
TarlanSB <tarlanworkmail@gmail.com>
2023-11-05 01:05:32 +0300

    Рефакторинг кода - индексы заменены на searchKey тип Object - имя метода protected void add(Resume r) заменен на insertElement (Object searchKey, Resume r) - изменены методы save, delete, update, get - добавлены методы getExistingSearchKey, getNotExistingSearchKey, isExist

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -2,60 +2,57 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.exception.ExistStorageException;
 import ru.javawebinar.basejava.exception.NotExistStorageException;
-import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
-public abstract class AbstractStorage implements Storage{
+public abstract class AbstractStorage implements Storage {
 
     @Override
     public final void save(Resume r) {
-        if (isSizeLimit()) {
-            throw new StorageException("Storage overflow", r.getUuid());
-        } else if (getIndex(r.getUuid()) >= 0) {
-            throw new ExistStorageException(r.getUuid());
-        } else {
-            add(r);
-        }
+        insertElement(getExistingSearchKey(r.getUuid()), r);
     }
 
     @Override
     public final void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index < 0) {
-            throw new NotExistStorageException(uuid);
-        } else {
-            remove(index);
-        }
+        remove(getNotExistingSearchKey(uuid));
     }
 
     @Override
     public final void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index >= 0) {
-            updateResume(index, r);
-            System.out.println("Резюме с uuid \"" + r.getUuid() + "\" обновилось");
-        } else {
-            throw new NotExistStorageException(r.getUuid());
-        }
+        Object searchKey = r.getUuid();
+        updateResume(getNotExistingSearchKey(searchKey.toString()), r);
     }
 
     @Override
     public final Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (index >= 0) {
-            return getResumeByIndex(index);
-        }
-        throw new NotExistStorageException(uuid);
+        return getResumeByIndex(getNotExistingSearchKey(uuid));
     }
 
-    protected abstract Resume getResumeByIndex(int index);
 
-    protected abstract boolean isSizeLimit();
-    protected abstract int getIndex(String uuid);
+    private Object getExistingSearchKey(String uuid) {
+        Object searchKey = getSearchKey(uuid);
+        if (isExist(searchKey)) {
+            throw new ExistStorageException(uuid);
+        }
+        return searchKey;
+    }
 
-    protected abstract void add(Resume r);
+    private Object getNotExistingSearchKey(String uuid) {
+        Object searchKey = getSearchKey(uuid);
+        if (!isExist(searchKey)) {
+            throw new NotExistStorageException(uuid);
+        }
+        return searchKey;
+    }
 
-    protected abstract void remove(int index);
+    protected abstract Resume getResumeByIndex(Object searchKey);
 
-    protected abstract void updateResume(int index, Resume r);
+    protected abstract Object getSearchKey(String uuid);
+
+    protected abstract void insertElement(Object searchKey, Resume r);
+
+    protected abstract void remove(Object searchKey);
+
+    protected abstract void updateResume(Object searchKey, Resume r);
+
+    protected abstract boolean isExist(Object searchKey);
 }
