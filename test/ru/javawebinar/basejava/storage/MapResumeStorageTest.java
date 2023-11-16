bb336d37f4fff43755ef15eeccd72a866f621c82
TarlanSB <tarlanworkmail@gmail.com>
2023-11-17 00:37:35 +0300

    Метод getSearchKey перенесен в родительский тест класс

--- test/ru/javawebinar/basejava/storage/MapResumeStorageTest.java
+++ test/ru/javawebinar/basejava/storage/MapResumeStorageTest.java
@@ -1,22 +1,10 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.Assert;
-
-import org.junit.Test;
-
-
-public class MapResumeStorageTest  extends AbstractMapStorageTest {
+public class MapResumeStorageTest extends AbstractMapStorageTest {
 
     private final static MapResumeStorage storage = new MapResumeStorage();
 
-    private static final String UUID_1 = "uuid1";
-
     public MapResumeStorageTest() {
         super(storage);
-    }
-
-    @Test
-    public void getSearchKey() {
-        Assert.assertEquals(UUID_1, storage.getSearchKey(UUID_1));
     }
 }
--- test/ru/javawebinar/basejava/storage/MapUuidStorageTest.java
+++ test/ru/javawebinar/basejava/storage/MapUuidStorageTest.java
@@ -1,22 +1,10 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.Assert;
-
-import org.junit.Test;
-
 public class MapUuidStorageTest extends AbstractMapStorageTest {
 
     private final static MapUuidStorage storage = new MapUuidStorage();
 
-    private static final String UUID_1 = "uuid1";
-
     public MapUuidStorageTest() {
         super(storage);
     }
-
-    @Test
-    public void getSearchKey() {
-        Assert.assertEquals(UUID_1, storage.getSearchKey(UUID_1));
-    }
-
 }
