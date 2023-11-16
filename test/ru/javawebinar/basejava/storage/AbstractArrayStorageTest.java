01b3fd87250000ccd4122a31d834f0b77e634155
TarlanSB <tarlanworkmail@gmail.com>
2023-11-16 03:02:18 +0300

    Изменения в связи с добавлением поля fullName для класса Resume

--- test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractArrayStorageTest.java
@@ -7,6 +7,9 @@ import ru.javawebinar.basejava.exception.NotExistStorageException;
 import ru.javawebinar.basejava.exception.StorageException;
 import ru.javawebinar.basejava.model.Resume;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.fail;
 
 public abstract class AbstractArrayStorageTest {
@@ -14,19 +17,24 @@ public abstract class AbstractArrayStorageTest {
     private final Storage storage;
 
     private static final String UUID_1 = "uuid1";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final String FULL_NAME_1 = "fullName1";
+    private static final Resume RESUME_1 = new Resume(UUID_1, FULL_NAME_1);
 
     private static final String UUID_2 = "uuid2";
-    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final String FULL_NAME_2 = "fullName2";
+    private static final Resume RESUME_2 = new Resume(UUID_2, FULL_NAME_2);
 
     private static final String UUID_3 = "uuid3";
-    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final String FULL_NAME_3 = "fullName3";
+    private static final Resume RESUME_3 = new Resume(UUID_3, FULL_NAME_3);
 
     private static final String UUID_4 = "uuid4";
-    private static final Resume RESUME_4 = new Resume(UUID_4);
+    private static final String FULL_NAME_4 = "fullName4";
+    private static final Resume RESUME_4 = new Resume(UUID_4, FULL_NAME_4);
 
     private static final int SIZE = 3;
     private static final String UUID_NOT_EXIST = "dummy";
+    private static final String UPDATE_FULL_NAME = "updateFullName";
 
     public AbstractArrayStorageTest(Storage storage) {
         this.storage = storage;
@@ -48,7 +56,7 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void update() {
-        Resume r = new Resume(UUID_2);
+        Resume r = new Resume(UUID_2, UPDATE_FULL_NAME);
         storage.update(r);
         Assert.assertSame(r, storage.get(UUID_2));
     }
@@ -77,13 +85,14 @@ public abstract class AbstractArrayStorageTest {
     public void clear() {
         storage.clear();
         assertSize(0);
-        Assert.assertArrayEquals(new Resume[]{}, storage.getAll());
+        Assert.assertArrayEquals(new Resume[]{}, storage.getAllSorted().toArray(new Resume[0]));
     }
 
     @Test
-    public void getAll() {
-        Resume[] expected = {RESUME_1, RESUME_2, RESUME_3};
-        Assert.assertArrayEquals(expected, storage.getAll());
+    public void getAllSorted()  {
+        List<Resume> expected = Arrays.asList(RESUME_1, RESUME_2, RESUME_3);
+        Assert.assertEquals(3, SIZE);
+        Assert.assertEquals(expected, storage.getAllSorted());
     }
 
     @Test(expected = NotExistStorageException.class)
@@ -102,11 +111,11 @@ public abstract class AbstractArrayStorageTest {
     public void saveOverflow() {
         try {
             for (int i = 3; i < AbstractArrayStorage.STORAGE_LIMIT; i++) {
-                storage.save(new Resume());
+                storage.save(new Resume("FullName_" + i));
             }
         } catch (StorageException e) {
             fail("Overflow has happen early");
         }
-        storage.save(new Resume());
+        storage.save(new Resume("Overflow"));
     }
 }
