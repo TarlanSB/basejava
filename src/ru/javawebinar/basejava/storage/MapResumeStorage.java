4ec726dd037cc8a1fb7f6a8d9d6e007fd13207a0
TarlanSB <tarlanworkmail@gmail.com>
2023-11-17 00:37:24 +0300

    Метод getSearchKey перенесен в родительский класс

--- src/ru/javawebinar/basejava/storage/MapResumeStorage.java
+++ src/ru/javawebinar/basejava/storage/MapResumeStorage.java
@@ -5,39 +5,17 @@ import ru.javawebinar.basejava.model.Resume;
 public class MapResumeStorage extends AbstractMapStorage {
 
     @Override
-    protected Resume doGet(Object resume) {
-        return storage.get(getResumeUuid(resume));
-    }
-
-    @Override
-    protected String getSearchKey(String uuid) {
-        return uuid;
-    }
-
-    @Override
-    protected void doSave(Object resume, Resume r) {
+    protected void doSave(Object searchKey, Resume r) {
         storage.put(r.getUuid(), r);
     }
 
     @Override
-    protected void doDelete(Object resume) {
-        storage.remove(getResumeUuid(resume));
-    }
-
-    @Override
-    protected void doUpdate(Object resume, Resume r) {
+    protected void doUpdate(Object searchKey, Resume r) {
         storage.put(r.getUuid(), r);
     }
 
     @Override
     protected boolean isExist(Object resume) {
-//       return resume == null;
-//       return storage.containsValue((Resume) resume);
-
         return storage.containsKey((String) resume);
-    }
-
-    private String getResumeUuid(Object resume) {
-        return ((Resume) resume).getUuid();
     }
 }
--- src/ru/javawebinar/basejava/storage/MapUuidStorage.java
+++ src/ru/javawebinar/basejava/storage/MapUuidStorage.java
@@ -5,23 +5,8 @@ import ru.javawebinar.basejava.model.Resume;
 public class MapUuidStorage extends AbstractMapStorage {
 
     @Override
-    protected Resume doGet(Object searchKey) {
-        return storage.get((String) searchKey);
-    }
-
-    @Override
-    protected String getSearchKey(String uuid) {
-        return uuid;
-    }
-
-    @Override
     protected void doSave(Object searchKey, Resume r) {
         storage.put((String) searchKey, r);
-    }
-
-    @Override
-    protected void doDelete(Object searchKey) {
-        storage.remove((String) searchKey);
     }
 
     @Override
