d3b49b25f03f73a9f5d9258af7d708d7a26bb03d
TarlanSB <tarlanworkmail@gmail.com>
2023-11-20 23:39:32 +0300

    Методы getSearchKey, doGet, doDelete перенесены из класса родителя с другой реализацией; изменение логики метода isExist

--- src/ru/javawebinar/basejava/storage/MapResumeStorage.java
+++ src/ru/javawebinar/basejava/storage/MapResumeStorage.java
@@ -2,7 +2,8 @@ package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.model.Resume;
 
-public class MapResumeStorage extends AbstractMapStorage {
+public class
+MapResumeStorage extends AbstractMapStorage {
 
     @Override
     protected void doSave(Object searchKey, Resume r) {
@@ -16,6 +17,22 @@ public class MapResumeStorage extends AbstractMapStorage {
 
     @Override
     protected boolean isExist(Object resume) {
-        return storage.containsKey((String) resume);
+        return storage.containsValue((Resume) resume);
+    }
+
+    @Override
+    public Object getSearchKey(String uuid) {
+        return storage.get(uuid);
+    }
+
+    @Override
+    protected Resume doGet(Object resume) {
+        return (Resume) resume;
+    }
+
+    @Override
+    protected void doDelete(Object resume) {
+        storage.remove(((Resume) resume).getUuid());
     }
 }
+
