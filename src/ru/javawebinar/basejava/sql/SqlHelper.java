202cf47db97c00c7ab1a42c17ee696480bff5929
TarlanSB <tarlanworkmail@gmail.com>
2024-01-26 00:02:12 +0300

    HW_13 добавлен метод executeException

--- src/ru/javawebinar/basejava/sql/SqlHelper.java
+++ src/ru/javawebinar/basejava/sql/SqlHelper.java
@@ -1,5 +1,9 @@
 package ru.javawebinar.basejava.sql;
 
+import org.postgresql.util.PSQLException;
+import ru.javawebinar.basejava.exception.ExistStorageException;
+import ru.javawebinar.basejava.exception.StorageException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -11,21 +15,23 @@ public class SqlHelper {
         this.connectionFactory = connectionFactory;
     }
 
-    public void execute(String sqlQuery) {
-        try (Connection conn = connectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sqlQuery)) {
-            ps.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public <T> T execute(String sqlQuery, SqlHelperImpl<T> executor) {
         try (Connection conn = connectionFactory.getConnection();
              PreparedStatement ps = conn.prepareStatement(sqlQuery)) {
             return executor.execute(ps);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+           throw executeException(e);
         }
     }
+
+    private StorageException executeException(SQLException e) {
+        if (e instanceof PSQLException) {
+            if (e.getSQLState().equals("23505")) {
+                return new ExistStorageException(null);
+            }
+        }
+        return new StorageException(e);
+    }
 }
+
+
