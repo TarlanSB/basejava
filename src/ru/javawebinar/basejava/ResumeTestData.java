ded2e3f1bffb4cb175e149f4898aa61e4b886c6c
TarlanSB <tarlanworkmail@gmail.com>
2024-02-16 02:10:02 +0300

    HW16

--- src/ru/javawebinar/basejava/ResumeTestData.java
+++ src/ru/javawebinar/basejava/ResumeTestData.java
@@ -2,7 +2,6 @@ package ru.javawebinar.basejava;
 
 import ru.javawebinar.basejava.model.*;
 
-import java.time.LocalDate;
 import java.time.Month;
 import java.util.*;
 
@@ -11,59 +10,75 @@ import static ru.javawebinar.basejava.util.DateUtil.of;
 
 public class ResumeTestData {
 
-    private final AbstractSection personal = new TextSection("Аналитический склад ума, сильная логика, креативность, " +
-            "инициативность. Пурист кода и архитектуры.");
+    private final AbstractSection personal = new TextSection("Аналитический склад ума, быстрая обучаемость, " +
+            "целеустремленность, стрессоустойчивость, аккуратность, без вредных привычек");
 
-    private final AbstractSection objective = new TextSection("Ведущий стажировок и корпоративного обучения по Java Web " +
-            "и Enterprise технологиям");
+    private final AbstractSection objective = new TextSection("Ведущий инженер проектировщик");
 
-    private final AbstractSection achievements = new ListSection(Arrays.asList("Организация команды и успешная реализация " +
-                    "Java проектов для сторонних заказчиков: приложения автопарк на стеке Spring Cloud/микросервисы, " +
-                    "система мониторинга показателей спортсменов на Spring Boot, участие в проекте МЭШ на Play-2, " +
-                    "многомодульный Spring Boot + Vaadin проект для комплексных DIY смет",
-            "\n С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven." +
-                    " Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\"." +
-                    " Организация онлайн стажировок и ведение проектов. Более 3500 выпускников.",
-            "\n Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с " +
-                    "Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.")
+    private final AbstractSection achievements = new ListSection(Arrays.asList("РАЗРАБОТКА тома \"МЕРОПРИЯТИЯ ПО ЗАЩИТЕ" +
+                    " ОТ ШУМА И ВИБРАЦИИ\"",
+            "Расчет акустическог воздействия от точечных, линейных, полигональных источников шума",
+            "Расчет проникающего внешнего шума в помещение",
+            "Построение шумовых карт воздействия источников шума на оприлегающию территрию",
+            "ОПРЕДЕЛЕНИЕ размеров границ расчетной САНИТАРНО-ЗАЩИТНОЙ ЗОНЫ",
+            "Расчет уровней зувукового давления в помещениях",
+            "РАЗРАБОТКА МЕРОПРИЯТИЙ по защите от шума",
+            "ПРОХОЖДЕНИЕ ГГЭ и ГЭЭ")
     );
-    private final AbstractSection qualifications = new ListSection(Arrays.asList("JEE AS: GlassFish (v2.1, v3), OC4J, " +
-                    "JBoss, Tomcat, Jetty, WebLogic, WSO2",
-            "\nVersion control: Subversion, Git, Mercury, ClearCase, Perforce",
-            "\nDB: PostgreSQL(наследование, pgplsql, PL/Python),Redis (Jedis),H2,Oracle,MySQL,SQLite, MS SQL, HSQLDB",
-            "\nLanguages:Java,Scala,Python/Jython/PL-Python,JavaScript,Groovy")
+    private final AbstractSection qualifications = new ListSection(Arrays.asList("АРМ Акустика, AutoCAD",
+            "Java 8, IntelliJ IDEA, GitHub/Git, Сервлеты, JSP, JSTL, Tomcat, JUnit, PostgreSQL, GSON, JAXB")
     );
 
-    private final LocalDate startDate1 = of(2013, Month.OCTOBER);
-    private final LocalDate endDate1 = NOW;
-    private final Organization.Position position1 = new Organization.Position(startDate1, endDate1, "Автор проекта.",
-            "Создание, организация и проведение Java онлайн проектов и стажировок");
+    private final Organization organization1 = new Organization("АО ГСПИ", "https://www.aogspi.ru//",
+            new Organization.Position(of(2013, Month.SEPTEMBER), NOW, "Инженер-проектировщик.",
+                    "РАЗРАБОТКА тома \"МЕРОПРИЯТИЯ ПО ЗАЩИТЕ ОТ ШУМА И ВИБРАЦИИ\""));
 
-    private final Organization organization1 = new Organization("Java Online Projects", "http://javaops.ru/",
-            position1);
+    private final Organization organization2 = new Organization("ООО ДТ-ФОТО", "",
+            new Organization.Position(of(2010, Month.JANUARY), of(2013, Month.SEPTEMBER), "Менеджер",
+                    ""));
 
-    private final LocalDate startDate2 = of(2013, Month.OCTOBER);
-    private final LocalDate endDate2 = of(2016, Month.OCTOBER);
-    private final Organization.Position position2 = new Organization.Position(startDate2, endDate2, "Старший разработчик (backend)",
-            "Проектирование и разработка онлайн платформы управления проектами Wrike (Java 8 API, Maven, " +
-                    "Spring, MyBatis, Guava, Vaadin, PostgreSQL, Redis). Двухфакторная аутентификация, " +
-                    "авторизация по OAuth1, OAuth2, JWT SSO.");
+    private final Organization organization3 = new Organization("ООО КПаскаль", "",
+            new Organization.Position(of(2010, Month.MARCH), of(2013, Month.JANUARY), "Помошник менеджера",
+                    ""));
 
-    private final Organization organization2 = new Organization("Wrike", "https://www.wrike.com/", position2);
+    //Собираем experience
+    protected AbstractSection experience = new OrganizationSection(organization1, organization2, organization3);
 
-    protected AbstractSection experience = new OrganizationSection(organization1, organization2);
+    private final Organization organization4 = new Organization("java online project",
+            "https://javaops.ru/view/baejava",
+            new Organization.Position(of(2023, Month.SEPTEMBER), of(2024, Month.FEBRUARY), "BASEJAVA",
+                    ""));
 
-    private final LocalDate startDate3 = of(1993, Month.SEPTEMBER);
-    private final LocalDate endDate3 = of(1996, Month.JULY);
-    private final Organization.Position position3 = new Organization.Position(startDate3, endDate3, "Аспирантура (программист С, С++)", "");
-    private final LocalDate startDate4 = of(1987, Month.SEPTEMBER);
-    private final LocalDate endDate4 = of(1993, Month.JULY);
-    private final Organization.Position position4 = new Organization.Position(startDate4, endDate4, "Инженер (программист Fortran, C)", "");
+    private final Organization organization5 = new Organization("UDEMY", "https://www.udemy.com/",
+            new Organization.Position(of(2024, Month.FEBRUARY), of(2024, Month.FEBRUARY), "Beginner's " +
+                    "Guide to SQL and PostgreSQL", ""));
 
-    private final Organization organization3 = new Organization("Санкт-Петербургский национальный исследовательский университет " +
-            "информационныхтехнологий, механики и оптики", "http://www.ifmo.ru/", position3, position4);
+    private final Organization organization6 = new Organization("java online project",
+            "https://javaops.ru/view/startjava",
+            new Organization.Position(of(2023, Month.MAY), of(2023, Month.SEPTEMBER), "STARTJAVA",
+                    "Mastered the syntax of Java, arrays, OOP, working with the console, the Git basics, SQL and" +
+                            " PostgreSQL"));
 
-    protected AbstractSection education = new OrganizationSection(organization3);
+    private final Organization organization7 = new Organization("UDEMY", "https://www.udemy.com/",
+            new Organization.Position(of(2023, Month.JANUARY), of(2023, Month.JANUARY), "JDBC",
+                    ""));
+
+    private final Organization organization8 = new Organization("UDEMY", "https://www.udemy.com/",
+            new Organization.Position(of(2022, Month.SEPTEMBER), of(2022, Month.OCTOBER), "Spring для " +
+                    "начинающих", ""));
+
+    private final Organization organization9 = new Organization("UDEMY", "https://www.udemy.com/",
+            new Organization.Position(of(2021, Month.OCTOBER), of(2021, Month.DECEMBER), "Java(Джава) " +
+                    "для начинающих: с нуля до сертификата Oracle", ""));
+
+    private final Organization organization10 = new Organization("Московский государственный университет " +
+            "природообустройства", "",
+            new Organization.Position(of(2003, Month.SEPTEMBER), of(2008, Month.JULY), "Инженер-механик",
+                    ""));
+
+    //Собираем education
+    protected AbstractSection education = new OrganizationSection(organization4, organization5, organization6,
+            organization7, organization8, organization9, organization10);
 
     public ResumeTestData() {
     }
--- web/WEB-INF/jsp/view.jsp
+++ web/WEB-INF/jsp/view.jsp
@@ -1,4 +1,5 @@
-
+<%@ page import="ru.javawebinar.basejava.model.ListSection" %>
+<%@ page import="ru.javawebinar.basejava.model.TextSection" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
@@ -11,7 +12,7 @@
 <body>
 <jsp:include page="fragments/header.jsp"/>
 <section>
-    <h2>${resume.fullName}&nbsp;<a href="resume?uuid=${resume.uuid}&action=edit"><img src="img/pencil.png"></a></h2>
+    <h1>${resume.fullName}&nbsp;<a href="resume?uuid=${resume.uuid}&action=edit"><img src="img/pencil.png"></a></h1>
     <p>
         <c:forEach var="contactEntry" items="${resume.contacts}">
             <jsp:useBean id="contactEntry"
@@ -19,6 +20,48 @@
                 <%=contactEntry.getKey().toHtml(contactEntry.getValue())%><br/>
         </c:forEach>
     <p>
+    <hr>
+    <table cellpadding="2">
+        <c:forEach var="sectionEntry" items="${resume.sections}">
+            <jsp:useBean id="sectionEntry"
+                         type="java.util.Map.Entry<ru.javawebinar.basejava.model.SectionType, ru.javawebinar.basejava.model.AbstractSection>"/>
+            <c:set var="type" value="${sectionEntry.key}"/>
+            <c:set var="section" value="${sectionEntry.value}"/>
+            <jsp:useBean id="section" type="ru.javawebinar.basejava.model.AbstractSection"/>
+            <tr>
+                <td colspan="2"><h2><a name="type.name">${type.title}</a></h2></td>
+            </tr>
+            <c:choose>
+                <c:when test="${type=='OBJECTIVE'}">
+                    <tr>
+                        <td colspan="2">
+                            <h3><%=((TextSection) section).getText()%></h3>
+                        </td>
+                    </tr>
+                </c:when>
+                <c:when test="${type=='PERSONAL'}">
+                    <tr>
+                        <td colspan="2">
+                            <%=((TextSection) section).getText()%>
+                        </td>
+                    </tr>
+                </c:when>
+                <c:when test="${type=='QUALIFICATIONS' || type=='ACHIEVEMENT'}">
+                    <tr>
+                        <td colspan="2">
+                            <ul>
+                                <c:forEach var="item" items="<%=((ListSection) section).getList()%>">
+                                    <li>${item}</li>
+                                </c:forEach>
+                            </ul>
+                        </td>
+                    </tr>
+                </c:when>
+            </c:choose>
+        </c:forEach>
+    </table>
+    <br/>
+    <button onclick="window.history.back()">ОК</button>
 </section>
 <jsp:include page="fragments/footer.jsp"/>
 </body>
