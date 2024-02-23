f0cbd14b4064796088980c79d43a91889d89b91d
TarlanSB <tarlanworkmail@gmail.com>
2024-02-23 03:33:35 +0300

    immutable uuids

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -7,7 +7,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 public class Config {
     private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
@@ -16,6 +18,11 @@ public class Config {
     private final File storageDir;
     private final Storage storage;
     private static final String STORAGE_DIR = "storage.dir";
+
+    private final Set<String> immutableUuids = new HashSet<>() {{  // for JDK 9+: Set.of("111", "222");
+        add("11111111-1111-1111-1111-111111111111");
+        add("22222222-2222-2222-2222-222222222222");
+    }};
 
     public static Config get() {
         return INSTANCE;
@@ -47,5 +54,14 @@ public class Config {
             throw new IllegalStateException(homeDir + " is not directory");
         }
         return homeDir;
+    }
+
+    public boolean isImmutable(String uuids) {
+        return immutableUuids.contains(uuids);
+    }
+
+    public void checkImmutable(String uuids) {
+        if (immutableUuids.contains(uuids))
+            throw new RuntimeException("Зарезервированные резюме нельзя менять");
     }
 }
